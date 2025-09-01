e1f7efd2e46efca1d2b3af15dc4f2305c6a8cab8
jordanbmnt <jordanbmnt@gmail.com>
2025-09-01 23:38:59 +0200

    Added EmployeeFilter Model

---
+++ Models/EmployeeFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HR_Administration_System.Models
+{
+    public class EmployeeFilter
+    {
+        public string Department { get; set; }
+        public string Status { get; set; }
+        public string Manager { get; set; }
+    }
+}
