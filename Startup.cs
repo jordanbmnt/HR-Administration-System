a93ae42246b5e73a9d312413393d2cdce631b49b
jordanbmnt <jordanbmnt@gmail.com>
2025-09-01 22:59:17 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HR_Administration_System.Startup))]
+namespace HR_Administration_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
