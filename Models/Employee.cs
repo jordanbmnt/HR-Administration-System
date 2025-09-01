bf8447401cc3905dd7658e91f638cd3dd67d4ae8
jordanbmnt <jordanbmnt@gmail.com>
2025-09-01 23:37:46 +0200

    Added Employee Model

---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HR_Administration_System.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string Manager { get; set; }
+        public string Status { get; set; }
+    }
+}
