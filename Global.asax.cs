33a94896b4aef7c871f2468e57633d4cb56195d7
jordanbmnt <jordanbmnt@gmail.com>
2025-09-04 23:11:45 +0200

    Added DB initializer to Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using HR_Administration_System.Models;
 
 namespace HR_Administration_System
 {
@@ -12,6 +14,9 @@ namespace HR_Administration_System
     {
         protected void Application_Start()
         {
+            // Set database initializer
+            Database.SetInitializer(new DbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
