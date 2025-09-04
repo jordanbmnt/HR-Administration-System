f41a2dcaed8d6acbce9f1608afd72386179f6ef0
jordanbmnt <jordanbmnt@gmail.com>
2025-09-04 23:14:41 +0200

    added custom Auth attributes

---
+++ Attributes/CustomAuthorizeAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HR_Administration_System.Attributes
+{
+    public class CustomAuthorizeAttribute
+    {
+    }
+}
